66a04c84258e99428f517058bf1aa76ded54ff84
DenisMartsenyuk <denmarc@bk.ru>
2020-12-19 22:47:01 +0300

    Final commit, I hope

--- src/main/java/ru/lab/weblab4/controller/PointController.java
+++ src/main/java/ru/lab/weblab4/controller/PointController.java
@@ -3,14 +3,11 @@ package ru.lab.weblab4.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import ru.lab.weblab4.dto.PointRequestDto;
 import ru.lab.weblab4.dto.PointResponseDto;
 import ru.lab.weblab4.service.PointService;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
